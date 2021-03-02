b42468399bf7df8d27a7e7b0c0ce682a0d3439d8
刘泽群 <liuzequn.corn@bytedance.com>
2021-03-02 10:38:12 +0800

    rm cmake folder

---
+++ NetFilterHook.h
@@ -0,0 +1,17 @@
+//
+// Created by admin on 2021/2/24.
+//
+
+#ifndef UDPSPEEDER_REGISTERNETHOOK_H
+#define UDPSPEEDER_REGISTERNETHOOK_H
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+
+void registerNetHook(int port) {
+
+}
+
+#endif //UDPSPEEDER_REGISTERNETHOOK_H
